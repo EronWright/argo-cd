adce8dfab41dda0d49aa667fd0de70aef6a35915
Soumya Ghosh Dastidar <44349253+gdsoumya@users.noreply.github.com>
2024-07-12 22:45:40 +0530

    feat: add destination metadata for argocd UI repo calls (#53)
    Signed-off-by: Soumya Ghosh Dastidar <gdsoumya@gmail.com>

--- server/application/application.go
+++ server/application/application.go
@@ -15,8 +15,6 @@ import (
 	"strings"
 	"time"
 
-	cacheutil "github.com/argoproj/argo-cd/v3/util/cache"
-
 	kubecache "github.com/argoproj/argo-cd/gitops-engine/pkg/cache"
 	"github.com/argoproj/argo-cd/gitops-engine/pkg/diff"
 	"github.com/argoproj/argo-cd/gitops-engine/pkg/health"
@@ -54,6 +52,7 @@ import (
 	"github.com/argoproj/argo-cd/v3/server/deeplinks"
 	applog "github.com/argoproj/argo-cd/v3/util/app/log"
 	"github.com/argoproj/argo-cd/v3/util/argo"
+	cacheutil "github.com/argoproj/argo-cd/v3/util/cache"
 	"github.com/argoproj/argo-cd/v3/util/collections"
 	"github.com/argoproj/argo-cd/v3/util/db"
 	"github.com/argoproj/argo-cd/v3/util/env"
